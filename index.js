01299582c55348f82a56245d54a93f62ed485bf0
Fatih Kadir Akın <fka@fatihak.in>
2015-01-19 01:01:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-module.exports = function (fn) {
-  var name = fn.name;
-  // If there's no name of a function, it's not possible to uniform it.
-  if (!name) { throw "Anonymous functions cannot be uniformed."; }
-  
-  // Overloading existing functions may cause problems.
-  if (Object.prototype[name]) {
-    throw "To uniform an existing function may hurt your feelings.";
-  }
-  
-  // Register function to the Object.
-  Object.prototype[name] = function () {
-    var args = Array.prototype.slice.call(arguments, 0);
-    // Add current value to the arguments list.
-    return fn.apply(fn, [this.valueOf()].concat(args));
+function ufcs() {
+  for (var i = 0; i < arguments.length; i++) {
+    var fn = arguments[i];
+    if (typeof fn === "function") {
+      var name = fn.name || fn.alias;
+      if (!name) { throw "Anonymous functions cannot be uniformed."; }
+      if (Object.prototype[name]) {
+        throw "To uniform an existing function may hurt your feelings.";
+      }
+      Object.prototype[name] = function () {
+        var args = Array.prototype.slice.call(arguments, 0);
+        return fn.apply(fn, [this.valueOf()].concat(args));
+      }
+    } else if (typeof fn === "object") {
+      for (var method in fn) {
+        var impl = fn[method];
+        if (typeof impl === "function" && fn.hasOwnProperty(method)) {
+          impl.alias = method;
+          ufcs(impl);
+        }
+      }
+    }
   }
 };
+
+module.export = ufcs;
